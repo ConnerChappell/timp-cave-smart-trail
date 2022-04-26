49cc1e26a032c59a5b8063372b01c4932bc7673b
Conner Chappell <conner.chappell123@gmail.com>
2022-04-25 18:05:24 -0600

    Added indigenous experience placeholder

---
+++ indigenous-experience/js/indigenous-experience.js
@@ -0,0 +1,17 @@
+///////////////////////// selectors
+const body = document.querySelector('body')
+const modal = document.querySelector('#welcome-modal')
+const closeModalBtn = document.querySelector('#close-modal-btn')
+
+
+
+
+// click event listener that closes welcome modal
+closeModalBtn.addEventListener('click', () => {
+    modal.classList.add('fade-out')
+    
+    setTimeout(function() {
+        modal.style.display = 'none'
+        modal.classList.remove('fade-out')
+    }, 300)
+})
